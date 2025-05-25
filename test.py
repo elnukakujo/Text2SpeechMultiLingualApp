5a60d6fdce61f5bd9e8dee9166cfe2541440c551
elnukakujo <jagernoe1s3@hotmail.com>
2025-05-25 14:59:49 +0200

    Collected more voice samples, and generated more chinese and japanese speeches

--- test.py
+++ test.py
@@ -3,17 +3,26 @@ from TTS.api import TTS
 xtts_v2 = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2")
 
 xtts_v2.tts_to_file(
-    "こんにちは、お元気ですか？",
-    speaker_wav="speaker_samples/japanese_male.wav",
-    file_path="generated_samples/japanese_male.wav",
-    language="ja",
-    emotion="happy"
-)
-
-xtts_v2.tts_to_file(
-    "你好，你好吗？",
-    speaker_wav="speaker_samples/japanese_male.wav",
-    file_path="generated_samples/chinese_male.wav",
+    "你把门打开吧！我进不来！",
+    speaker_wav="speaker_samples/mingyi_f.wav",
+    file_path="generated_samples/mingyi_chinese.wav",
     language="zh",
-    emotion="happy"
+    emotion="happy",
+    speed=0.95,
 )
+xtts_v2.tts_to_file(
+    "Salut! Comment ça va? Tu es en train de programme?",
+    speaker_wav="speaker_samples/mingyi_f.wav",
+    file_path="generated_samples/mingyi_french.wav",
+    language="fr",
+    emotion="happy",
+    speed=0.95,
+)
+"""xtts_v2.tts_to_file(
+    "おはよう！元気で？今コディングをして要る？",
+    speaker_wav="speaker_samples/jean_m.wav",
+    file_path="generated_samples/jean_japanese.wav",
+    language="ja",
+    emotion="happy",
+    speed=0.95,
+)"""
